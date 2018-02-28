e1564430641afbeb9548909844ce446d270a2c44
Tim Noell <timn@ad.exigo.com>
2018-02-28 16:10:33 -0600

    Add project files.

---
+++ Exigo-ClientReporting/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exigo_ClientReporting
+{
+    public class Class1
+    {
+    }
+}
